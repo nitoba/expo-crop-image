446a42b31e735cbf19d49bfa34fda74ff330e294
Bruno Alves <bruno@sthorm.io>
2022-11-19 00:03:11 -0300

    feat: add github actions workflow

--- dist/context/editor.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EditorContext = void 0;
-const react_1 = require("react");
-exports.EditorContext = (0, react_1.createContext)({});
-//# sourceMappingURL=editor.js.map
